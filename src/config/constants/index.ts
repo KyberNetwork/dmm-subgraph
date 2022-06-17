76fbec0eae877afedb81a9408194ff0448273b8b
Nguyen Van Viet <vietnv1304@gmail.com>
2022-06-17 13:43:15 +0700

    merge legacy static fee

--- src/config/constants/index.ts
+++ src/config/constants/index.ts
@@ -4,30 +4,30 @@ export const ADDRESS_ZERO = '0x0000000000000000000000000000000000000000'
 
 export const ADDRESS_LOCK = '0xffffffffffffffffffffffffffffffffffffffff'
 
-export const DMM_DYNAMIC_FEE_FACTORY_ADDRESS = '0x0639542a5cd99bd5f4e85f58cb1f61d8fbe32de9'
-export const DMM_STATIC_FEE_FACTORY_ADDRESS = '0xb332f6145a5b064f58ff9793ba3523245f8fafac'
+export const DMM_DYNAMIC_FEE_FACTORY_ADDRESS = '0x833e4083b7ae46cea85695c4f7ed25cdad8886de'
+export const DMM_STATIC_FEE_FACTORY_ADDRESS = '0x1c758af0688502e49140230f6b0ebd376d429be5'
 export const DMM_STATIC_FEE_FACTORY_LEGACY_ADDRESS = ''
 
-export const ETH_PRICING_POOLS = '0x568618004062e83fc286fb322f80a4f2814e80a7|0x26b8d1680b8450dda51fa7d4753cdca9766717bd|0xb26a8395ba1997a4c9a47d1589234bcd3ff468f3'
+export const ETH_PRICING_POOLS = '0xd478953d5572f829f457a5052580cbeaee36c1aa|0x20d6b227f4a5a2a13d520329f01bb1f8f9d2d628|0xf8467ef9de03e83b5a778ac858ea5c2d1fc47188|0xce9874c42dce7fffbe5e48b026ff1182733266cb'
 
 // minimum liquidity required to count towards tracked volume for pairs with small # of Lps
-export let MINIMUM_USD_THRESHOLD_NEW_PAIRS = BigDecimal.fromString('1')
+export let MINIMUM_USD_THRESHOLD_NEW_PAIRS = BigDecimal.fromString('4000')
 
 // minimum liquidity for price to get tracked
-export let MINIMUM_LIQUIDITY_THRESHOLD_ETH = BigDecimal.fromString('0') // default is 2
+export let MINIMUM_LIQUIDITY_THRESHOLD_ETH = BigDecimal.fromString('2') // default is 2
 
-export const WRAPPED_NATIVE_ADDRESS = '0xc778417e063141139fce010982780140aa0cd5ab'
+export const WRAPPED_NATIVE_ADDRESS = '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2'
 
-export const KNC_ADDRESS = '0x8b4ddf9f13f382aff76d262f6c8c50e6d7961b94'
+export const KNC_ADDRESS = '0xdefa4e8a7bcba345f687a2f1456f5edd9ce97202'
 export const KNC_NAME = 'Kyber Network'
 export const KNC_SYMBOL = 'KNC'
 
-export const KNCL_ADDRESS = '0x7b2810576aa1cce68f2b118cef1f36467c648f92'
+export const KNCL_ADDRESS = '0xdd974d5c2e2928dea5f71b9825b8b646686bd200'
 export const KNCL_NAME = 'Kyber Network Legacy'
 export const KNCL_SYMBOL = 'KNCL'
 
 export let FACTORY_BPS = BigDecimal.fromString('10000')
 
-export let NETWORK = 'ropsten'
+export let NETWORK = 'mainnet'
 
-export let WHITELISTED_TOKENS = '0xc778417e063141139fce010982780140aa0cd5ab,0xad6d458402f60fd3bd25163575031acdce07538d,0x068b43f7f2f2c6a662c36e201144ae45f7a1c040,0x65bd1f48f1dd07bb285a3715c588f75684128ace,0x8b4ddf9f13f382aff76d262f6c8c50e6d7961b94'
+export let WHITELISTED_TOKENS = '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2,0x6b175474e89094c44da98b954eedeac495271d0f,0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48,0xdac17f958d2ee523a2206206994597c13d831ec7,0xdefa4e8a7bcba345f687a2f1456f5edd9ce97202'
--- src/mappings/factory.ts
+++ src/mappings/factory.ts
@@ -6,6 +6,7 @@ import {
   SetFeeConfiguration
 } from '../types/DmmDynamicFeeFactory/DmmDynamicFeeFactory'
 import { PoolCreated as StaticFeePoolCreated } from '../types/DmmStaticFeeFactory/DmmStaticFeeFactory'
+import { PoolCreated as StaticFeePoolCreatedLegacy } from '../types/DmmStaticFeeFactoryLegacy/DmmStaticFeeFactoryLegacy'
 import { DMM_STATIC_FEE_FACTORY_ADDRESS, DMM_STATIC_FEE_FACTORY_LEGACY_ADDRESS } from '../config/constants'
 
 import { Pool as PoolTemplate } from '../types/templates'
@@ -39,6 +40,136 @@ function createOrLoadFactory(address: string): DmmFactory {
   }
 
   return factory as DmmFactory
+}
+
+export function handlePoolCreatedLegacy(event: StaticFeePoolCreatedLegacy): void {
+  log.debug('------run to handle PoolCreated event ------ ', [])
+
+  let factory = createOrLoadFactory(event.address.toHexString())
+
+  factory.poolCount = factory.poolCount + 1
+  factory.save()
+
+  log.debug('------save factory success ------', [])
+
+  // create the tokens
+  let token0 = Token.load(event.params.token0.toHexString())
+  let token1 = Token.load(event.params.token1.toHexString())
+
+  // fetch info if null
+  if (token0 === null) {
+    token0 = new Token(event.params.token0.toHexString())
+    token0.symbol = fetchTokenSymbol(event.params.token0)
+    token0.name = fetchTokenName(event.params.token0)
+    token0.totalSupply = fetchTokenTotalSupply(event.params.token0)
+    token0.decimals = fetchTokenDecimals(event.params.token0)
+    token0.derivedETH = ZERO_BD
+    token0.tradeVolume = ZERO_BD
+    token0.tradeVolumeUSD = ZERO_BD
+    token0.untrackedVolumeUSD = ZERO_BD
+    token0.totalLiquidity = ZERO_BD
+    // token0.allPairs = []
+    token0.txCount = ZERO_BI
+    token0.save()
+
+    log.debug('------fetch token 0 success ------ ', [token0.id])
+  }
+
+  // fetch info if null
+  if (token1 === null) {
+    token1 = new Token(event.params.token1.toHexString())
+    token1.symbol = fetchTokenSymbol(event.params.token1)
+    token1.name = fetchTokenName(event.params.token1)
+    token1.totalSupply = fetchTokenTotalSupply(event.params.token1)
+    token1.decimals = fetchTokenDecimals(event.params.token1)
+    token1.derivedETH = ZERO_BD
+    token1.tradeVolume = ZERO_BD
+    token1.tradeVolumeUSD = ZERO_BD
+    token1.untrackedVolumeUSD = ZERO_BD
+    token1.totalLiquidity = ZERO_BD
+    // token1.allPairs = []
+    token1.txCount = ZERO_BI
+    token1.save()
+
+    log.debug('------fetch token 1 success ------ ', [token1.id])
+  }
+
+  let pool = new Pool(event.params.pool.toHexString())
+  let pairId = token0.id + '_' + token1.id
+  let pair = Pair.load(pairId)
+
+  if (pair == null) {
+    // create new pair
+    pair = new Pair(pairId)
+    pair.token0 = token0.id
+    pair.token1 = token1.id
+    pair.liquidityProviderCount = ZERO_BI
+    pair.createdAtTimestamp = event.block.timestamp
+    pair.createdAtBlockNumber = event.block.number
+    pair.txCount = ZERO_BI
+    pair.reserve0 = ZERO_BD
+    pair.reserve1 = ZERO_BD
+    pair.trackedReserveETH = ZERO_BD
+    pair.reserveETH = ZERO_BD
+    pair.reserveUSD = ZERO_BD
+    pair.totalSupply = ZERO_BD
+    pair.volumeToken0 = ZERO_BD
+    pair.volumeToken1 = ZERO_BD
+    pair.volumeUSD = ZERO_BD
+    pair.feeUSD = ZERO_BD
+    pair.untrackedFeeUSD = ZERO_BD
+    pair.untrackedVolumeUSD = ZERO_BD
+    pair.token0Price = ZERO_BD
+    pair.token1Price = ZERO_BD
+    pair.pools = []
+
+    factory.pairCount = factory.pairCount + 1
+    factory.save()
+  }
+  pair.pools = pair.pools.concat([pool.id])
+  pair.save()
+
+  pool.token0 = token0.id
+  pool.token1 = token1.id
+  pool.pair = pair.id
+  pool.liquidityProviderCount = ZERO_BI
+  pool.createdAtTimestamp = event.block.timestamp
+  pool.createdAtBlockNumber = event.block.number
+  pool.txCount = ZERO_BI
+  pool.reserve0 = ZERO_BD
+  pool.reserve1 = ZERO_BD
+  pool.vReserve0 = ZERO_BD
+  pool.vReserve1 = ZERO_BD
+  pool.trackedReserveETH = ZERO_BD
+  pool.amplifiedTrackedLiquidityEth = ZERO_BD
+  pool.reserveETH = ZERO_BD
+  pool.reserveUSD = ZERO_BD
+  pool.totalSupply = ZERO_BD
+  pool.volumeToken0 = ZERO_BD
+  pool.volumeToken1 = ZERO_BD
+  pool.volumeUSD = ZERO_BD
+  pool.feeUSD = ZERO_BD
+  pool.untrackedFeeUSD = ZERO_BD
+  pool.untrackedVolumeUSD = ZERO_BD
+  pool.token0Price = ZERO_BD
+  pool.token0PriceMin = ZERO_BD
+  pool.token0PriceMax = ZERO_BD
+
+  pool.token1Price = ZERO_BD
+  pool.token1PriceMin = ZERO_BD
+  pool.token1PriceMax = ZERO_BD
+
+  pool.liquidityPerRisk = ZERO_BD
+  pool.amp = event.params.ampBps.toBigDecimal()
+
+  let isStaticFeeLegacy = event.address.toHexString() == DMM_STATIC_FEE_FACTORY_LEGACY_ADDRESS
+  if (isStaticFeeLegacy) {
+    pool.fee = event.params.feeBps
+  }
+
+  pool.save()
+
+  PoolTemplate.create(event.params.pool)
 }
 
 export function handlePoolCreated(event: StaticFeePoolCreated): void {
@@ -169,7 +300,7 @@ export function handlePoolCreated(event: StaticFeePoolCreated): void {
   // TODO: Add uint16 fee
   // let isStaticFeeLegacy = event.address.toHexString() == DMM_STATIC_FEE_FACTORY_LEGACY_ADDRESS
   // if (isStaticFeeLegacy) {
-  //   pool.fee = (event.params as any).feeBps
+  //   pool.fee = (event as StaticFeePoolCreatedLegacy).params.feeBps
   // }
 
   pool.save()
